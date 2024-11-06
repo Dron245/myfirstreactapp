04a69bcc608790a2a799781b775c6e98dca5524f
Dron245 <Dron245@yandex.ru>
2024-11-06 08:44:57 +0300

    0611-0845

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import Block from "./Components/Block/Block";
-const Block2 = React.lazy(() => import("./Components/Block2"));
-// import Block from './Components/Block';
-// import Block2 from './Components/Block2';
+import React, { Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Block from './Components/Block/Block';
+const Block2 = React.lazy(() => import('./Components/Block2'));
+// import Block2 from './Components/Block2'
 function App() {
 	return (
 		<>
 			<Routes>
-				<Route path="/" element={
-					<Suspense fallback={<div>Идёт загрузка корзины...</div>}>
-						<Block/>
-					</Suspense>
-					}/>
-				<Route path="/qwe" element={<Block2/>}/>
+				<Route path='/' element={<Block />} />
+				<Route
+					path='/qwe'
+					element={
+						<Suspense fallback={<div>Идёт загрузка корзины...</div>}>
+							<Block2 />
+						</Suspense>
+					}
+				/>
 			</Routes>
 		</>
 	);
--- src/Components/Block/Block.jsx
+++ src/Components/Block/Block.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import styles from './Block.module.css'
+import { Link } from 'react-router-dom'
+// import { add } from '../math'
+// add()
+import("../math").then(math=>math.add(2,3))
 const Block = () => {
   return (
 	 <div className={styles.asd}>
 		Block
+		<Link to='/qwe'>
+			<button >lorem ipsum</button>
+		</Link>
 	 </div>
   )
 }
